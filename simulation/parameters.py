3dfacaf62d21a632298474316647f68ed2148c06
Alex Creiner <alexbcreiner@gmail.com>
2026-05-02 15:15:55 -0400

    various changes, still some mysterious stuff

--- simulation/parameters.py
+++ simulation/parameters.py
@@ -5,13 +5,13 @@ from numpy import array, ndarray
 @dataclass
 class Params:
     N_c: int = 5
-    N_S: int = 3000
-    N_h: int = 30
+    N_S: int = 10000
+    N_h: int = 100
     m_r: int = 5
     N_p: int = 5
     N_d: int = 5
     W: int = 5
     T: int = 8
     S: float = 0.01
-    v_ability: float = 0.05
+    v_ability: float = 0.0005
     is_logging: bool = False
